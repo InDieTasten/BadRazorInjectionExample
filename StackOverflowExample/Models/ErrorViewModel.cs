c27f34fde1934c749a3bd221e9cc1aaaac1f0537
InDieTasten <indietasten@gmail.com>
2018-09-29 14:51:18 +0200

    Add project files.

---
+++ StackOverflowExample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StackOverflowExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
